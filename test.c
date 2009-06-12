249b230ddaa942d973b40e96823e49752995af4b
Ondrej Certik <ondrej@certik.cz>
2009-06-12 13:24:05 -0600

    Prepare things for th_decode_headerin()

--- test.c
+++ test.c
@@ -3,6 +3,10 @@
 int main()
 {
     th_info info;
+    th_comment tc;
+    th_setup_info *setup;
+    ogg_packet op;
     th_info_init(&info);
+    //th_decode_headerin(&info, &tc, &setup, &op);
     return 0;
 }
