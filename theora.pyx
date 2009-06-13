94e021b7fdbe7643761b3e8d95ecb08d4f9931e8
Ondrej Certik <ondrej@certik.cz>
2009-06-12 19:05:40 -0600

    Headers are parsed correctly

--- theora.pyx
+++ theora.pyx
@@ -6,14 +6,25 @@ cdef extern from "stdlib.h":
 
 cdef extern from "theora/theoradec.h":
 
+    ctypedef unsigned int ogg_uint32_t
+
     ctypedef struct ogg_sync_state:
         pass
     ctypedef struct th_comment:
         pass
     ctypedef struct th_info:
-        pass
+        ogg_uint32_t  frame_width
+        ogg_uint32_t  frame_height
+        ogg_uint32_t  pic_width
+        ogg_uint32_t  pic_height
+        ogg_uint32_t  pic_x
+        ogg_uint32_t  pic_y
+        ogg_uint32_t  fps_numerator
+        ogg_uint32_t  fps_denominator
+        ogg_uint32_t  aspect_numerator
+        ogg_uint32_t  aspect_denominator
     ctypedef struct ogg_stream_state:
-        pass
+        long serialno
     ctypedef struct ogg_page:
         pass
     ctypedef struct ogg_packet:
@@ -75,7 +86,7 @@ cdef class Ogg:
                 print "done"
                 return
             while ogg_sync_pageout(&self._oy, &self._og) > 0:
-                if ogg_page_bos(&self._og) != 0:
+                if ogg_page_bos(&self._og) == 0:
                     if theora_p:
                         ogg_stream_pagein(&self._to, &self._og)
                     stateflag = False
@@ -108,4 +119,14 @@ cdef class Ogg:
                 if ret == 0:
                     print "End of file while searching for headers"
                     return
+        if self._ti.fps_denominator == 0:
+            raise Exception("fps_denominator is zero")
+        print "Ogg logical stream %lx is Theora %dx%d %.02f fps video\n" \
+            "Encoded frame content is %dx%d with %dx%d offset\n" \
+            "Aspect: %d:%d\n" % (
+            self._to.serialno, self._ti.pic_width, self._ti.pic_height,
+            float(self._ti.fps_numerator)/self._ti.fps_denominator,
+            self._ti.frame_width, self._ti.frame_height,
+            self._ti.pic_x, self._ti.pic_y,
+            self._ti.aspect_numerator, self._ti.aspect_denominator)
         print "ok"
