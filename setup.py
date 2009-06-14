030d95b9d8d2d48cb96f1bf55c1487f42d7b62e2
Ondrej Certik <ondrej@certik.cz>
2009-06-14 15:45:52 -0600

    setup.py: change 2 spaces indentation to 4 spaces

--- setup.py
+++ setup.py
@@ -8,23 +8,23 @@ __version__ = '0.1'
 
 # write default shout.pc path into environment if PKG_CONFIG_PATH is unset
 if not os.environ.has_key('PKG_CONFIG_PATH'):
-  os.environ['PKG_CONFIG_PATH'] = '/usr/local/lib/pkgconfig'
+    os.environ['PKG_CONFIG_PATH'] = '/usr/local/lib/pkgconfig'
 
 # Find shout compiler/linker flag via pkgconfig or shout-config
 if os.system('pkg-config --exists theoraenc theoradec 2> /dev/null') == 0:
-  pkgcfg = os.popen('pkg-config --cflags theoraenc theoradec')
-  cflags = pkgcfg.readline().strip()
-  pkgcfg.close()
-  pkgcfg = os.popen('pkg-config --libs theoraenc theoradec')
-  libs = pkgcfg.readline().strip()
-  pkgcfg.close()
+    pkgcfg = os.popen('pkg-config --cflags theoraenc theoradec')
+    cflags = pkgcfg.readline().strip()
+    pkgcfg.close()
+    pkgcfg = os.popen('pkg-config --libs theoraenc theoradec')
+    libs = pkgcfg.readline().strip()
+    pkgcfg.close()
 
 else:
-  if os.system('pkg-config --usage 2> /dev/null') == 0:
-    print "pkg-config could not find theoraenc theoradec: check PKG_CONFIG_PATH"
-  else:
-    print "pkg-config unavailable, build terminated"
-    sys.exit(1)
+    if os.system('pkg-config --usage 2> /dev/null') == 0:
+        print "pkg-config could not find theoraenc theoradec: check PKG_CONFIG_PATH"
+    else:
+        print "pkg-config unavailable, build terminated"
+        sys.exit(1)
 
 # there must be an easier way to set up these flags!
 iflags = [x[2:] for x in cflags.split() if x[0:2] == '-I']
