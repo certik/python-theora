1e26e7da44f40fa7ffa235432e710d864c7a5e03
Ondrej Certik <ondrej@certik.cz>
2009-06-14 23:06:05 -0600

    Testing infrastructure started

---
+++ tests/download_test_vidos.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+# Downloads the official theora test files, that we use for testing the python
+# wrappers and running doctests:
+
+WGET="wget -nc"
+
+# ROOT points to the tests directory:
+ROOT=`readlink -nf "$0" 2> /dev/null` || \
+ROOT=`readlink -n "$0" 2> /dev/null` || \
+ROOT=`realpath    "$0" 2> /dev/null` || \
+ROOT="$0"
+ROOT="${ROOT%/*}/"
+
+cd $ROOT
+mkdir -p videos
+cd videos
+$WGET http://v2v.cc/~j/theora_testsuite/320x240.ogg
+$WGET http://v2v.cc/~j/theora_testsuite/320x240.ogv
