58b51f949811a2279b76076e8416c84b27133336
Ondrej Certik <ondrej@certik.cz>
2009-06-14 23:21:52 -0600

    Few more tests added

--- tests/download_test_vidos.sh
+++ tests/download_test_vidos.sh
@@ -15,4 +15,6 @@ cd $ROOT
 mkdir -p videos
 cd videos
 $WGET http://v2v.cc/~j/theora_testsuite/320x240.ogg
-$WGET http://v2v.cc/~j/theora_testsuite/320x240.ogv
+#$WGET http://v2v.cc/~j/theora_testsuite/320x240.ogv
+$WGET http://v2v.cc/~j/theora_testsuite/videotestsrc-720x576-16-15.ogg
+$WGET http://v2v.cc/~j/theora_testsuite/offset_test.ogv
--- tests/test_decoder.py
+++ tests/test_decoder.py
@@ -1,13 +1,23 @@
 from theora import Theora
 
 test_file1 = "tests/videos/320x240.ogg"
-test_file2 = "tests/videos/320x240.ogv"
+test_file2 = "tests/videos/videotestsrc-720x576-16-15.ogg"
+test_file3 = "tests/videos/offset_test.ogv"
 
 def test_open1():
     t = Theora(test_file1)
+    assert t.width == 320
+    assert t.height == 240
 
 def test_open2():
     t = Theora(test_file2)
+    assert t.width == 720
+    assert t.height == 576
+
+def test_open3():
+    t = Theora(test_file3)
+    assert t.width == 512
+    assert t.height == 512
 
 def test_read_frame():
     t = Theora(test_file1)
