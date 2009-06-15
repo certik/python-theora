751729dda99c1483b0f88ac43bb3499c44f11f23
Ondrej Certik <ondrej@certik.cz>
2009-06-14 23:17:29 -0600

    read_frame and get_frame_data tests added

--- tests/test_decoder.py
+++ tests/test_decoder.py
@@ -8,3 +8,18 @@ def test_open1():
 
 def test_open2():
     t = Theora(test_file2)
+
+def test_read_frame():
+    t = Theora(test_file1)
+    t.read_frame()
+
+def test_get_frame_data():
+    t = Theora(test_file1)
+    t.read_frame()
+    data = t.get_frame_data()
+    assert isinstance(data, (list, tuple))
+    assert len(data) == 3
+    Y, Cb, Cr = data
+    assert Y.shape == (240, 320)
+    assert Cb.shape == (120, 160)
+    assert Cr.shape == (120, 160)
