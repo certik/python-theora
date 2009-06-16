1f08b776393adfe13335e48e40905a91a8e5d5c7
Ondrej Certik <ondrej@certik.cz>
2009-06-15 18:56:17 -0600

    RGB2YCbCr() function added to colors.py

--- colors.py
+++ colors.py
@@ -15,8 +15,17 @@ and readable, so it serves as a reference implementation.
 
 """
 
-from numpy import array, dot, round
+from numpy import array, dot, vectorize
 from scipy.linalg import inv
+
+def round_int(a):
+    return int(a+0.5)
+
+vround_int = vectorize(round_int)
+
+def round(A):
+    return vround_int(A)
+
 
 # parameters both Rec. 470M and Rec. 470BG:
 offset = array([16, 128, 128])
@@ -76,6 +85,12 @@ def YCbCr2RGB_precise(YCrCb):
     """
     return RGB_float2RGB(YPbPr2RGB_float(YCbCr2YPbPr(YCrCb)))
 
+def RGB2YCbCr_precise(RGB):
+    """
+    This is a precise, but slow version.
+    """
+    return YPbPr2YCbCr(RGB_float2YPbPr(RGB2RGB_float(RGB)))
+
 def YCbCr2RGB(YCbCr):
     """
     This is a fast version, that sometimes differes by 1.
@@ -86,11 +101,20 @@ def YCbCr2RGB(YCbCr):
     C = Y - 16
     D = Cb - 128
     E = Cr - 128
-    print C, D, E
-    print (298*C + 409*E + 128)
-    print (298*C + 409*E + 128) >> 8
 
     R = clip2((298*C + 409*E + 128) >> 8)
     G = clip2((298*C - 100*D - 208*E + 128) >> 8)
     B = clip2((298*C + 516*D + 128) >> 8)
     return array([R, G, B])
+
+def RGB2YCbCr(RGB):
+    """
+    This is a fast version, that sometimes differes by 1.
+
+    It can be easily cythonized.
+    """
+    R, G, B = RGB
+    Y = ( (  66 * R + 129 * G +  25 * B + 128) >> 8) +  16
+    Cb = ( ( -38 * R -  74 * G + 112 * B + 128) >> 8) + 128
+    Cr = ( ( 112 * R -  94 * G -  18 * B + 128) >> 8) + 128
+    return Y, Cb, Cr
